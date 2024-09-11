21336f052c188673d54dd9a19fc1f56b9aac4ffb
luca.stendardo <luca.stendardo@IT-MOBILE-02>
2024-09-11 16:01:17 +0200

    add tailwind & primevue

--- crm-task/src/main.ts
+++ crm-task/src/main.ts
@@ -1,11 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import { AuraBlue } from './utils/primevue/custom-theme'
+import './assets/style.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+
+// integrate primevue wich customized theme theme
+app.use(PrimeVue, {
+  theme: {
+    preset: AuraBlue
+  }
+})
 
 app.mount('#app')
