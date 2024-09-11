159b847d7f952143e1a494000d7ecb1ac802d4ba
luca.stendardo <luca.stendardo@IT-MOBILE-02>
2024-09-11 16:09:23 +0200

    add axios & i18n

--- crm-task/src/App.vue
+++ crm-task/src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import Button from 'primevue/button'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
 </script>
 
 <template>
-  <Button label="aaa" />
+  <Button :label="t('bla')" />
 
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
--- crm-task/src/main.ts
+++ crm-task/src/main.ts
@@ -1,9 +1,11 @@
+import './assets/style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import PrimeVue from 'primevue/config'
 import { AuraBlue } from './utils/primevue/custom-theme'
-import './assets/style.css'
+import { createI18n } from 'vue-i18n'
+import dictionaries from './i18n'
 
 const app = createApp(App)
 
@@ -15,5 +17,14 @@ app.use(PrimeVue, {
     preset: AuraBlue
   }
 })
+
+// configure i18n
+const i18n = createI18n({
+  locale: 'en-US',
+  fallbackLocale: 'en-US',
+  messages: dictionaries,
+  legacy: false
+})
+app.use(i18n)
 
 app.mount('#app')
